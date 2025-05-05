53b84d9d53e2d151dcd3d8074f22a5ec949f8b18
tessyelots <marek.smutny166@gmail.com>
2025-05-05 13:04:17 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -35,6 +35,9 @@ switch ($command) {
     "mongo" {
         mongo up
     }
+    "docker" {
+        docker build -t xsmutny/porodnica-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
